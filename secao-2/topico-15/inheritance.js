521c7b206e270cf59b834ff69588d5c78aa06a2f
Heitor Mariano <heitor.eumesmo@gmail.com>
2021-06-03 17:33:26 -0300

    revisão e melhoria de código

--- secao-2/topico-15/inheritance.js
+++ secao-2/topico-15/inheritance.js
@@ -1,5 +1,26 @@
-class Car {
-  constructor(brand) {
-    this.brand = brand;
+class Human {
+  constructor(gender) {
+    this.gender = gender;
+  }
+
+  printMyGender() {
+    console.log(this.gender);
   }
 }
+
+class Person extends Human {
+  constructor(firstName, lastName, age, gender) {
+    super(gender);
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const person01 = new Person('Carlos', 'Alencar', 28, 'Male');
+console.log(person01.getFullName());
+person01.printMyGender();

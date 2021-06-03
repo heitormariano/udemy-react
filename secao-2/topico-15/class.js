ad24772657814c0452c0deed0ce47479d9712cdc
Heitor Mariano <heitor.eumesmo@gmail.com>
2021-06-03 17:22:33 -0300

    melhoria de código com novas implementações

--- secao-2/topico-15/class.js
+++ secao-2/topico-15/class.js
@@ -1,9 +1,27 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+class Student {
+  constructor() {
+    this.name = 'Mateus';
+  }
+
+  printMyName() {
+    console.log(this.name);
   }
 }
 
-let myPerson = new Person('Heitor Camara', 33);
-console.log(`Name: ${myPerson.name}. Age: ${myPerson.age}`);
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const myStudent = new Student();
+myStudent.printMyName();
+
+const myPerson = new Person('Heitor', 'Camara', 34);
+console.log(`Full name: ${myPerson.getFullName()}. Age: ${myPerson.age}`);
